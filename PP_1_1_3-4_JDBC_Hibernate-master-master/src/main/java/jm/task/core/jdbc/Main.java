b8a77c0e33af577e5950ceeca6c6d348ba527dd8
Toltek91 <113970778+Toltek91@users.noreply.github.com>
2023-12-26 23:13:30 +0700

    приватизировано поле UserDao UserDaoHibernate убрал из интерфейсов throws SQLException; кодстайл

--- PP_1_1_3-4_JDBC_Hibernate-master-master/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate-master-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -3,12 +3,10 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.SQLException;
-
 public class Main {
     public static UserService userService = new UserServiceImpl();
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
         userService.createUsersTable();
         userService.saveUser("Shack", "Onel", (byte) 55);
--- PP_1_1_3-4_JDBC_Hibernate-master-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate-master-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,72 +1,52 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import java.sql.*;
+
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDao UserDaoJDBCImpl= new UserDaoJDBCImpl();
+    private final UserDao UserDaoJDBCImpl = new UserDaoJDBCImpl();
 
 
     @Override
     public void createUsersTable() {
-        try {
-            UserDaoJDBCImpl.createUsersTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        UserDaoJDBCImpl.createUsersTable();
 
     }
+
     @Override
     public void dropUsersTable() {
-        try {
-            UserDaoJDBCImpl.dropUsersTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        UserDaoJDBCImpl.dropUsersTable();
 
     }
+
     @Override
-    public void saveUser(String name, String lastName, byte age)  {
-        try {
-            UserDaoJDBCImpl.saveUser(name, lastName, age);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("User с именем – " + name +" добавлен в базу данных ");
+    public void saveUser(String name, String lastName, byte age) {
+        UserDaoJDBCImpl.saveUser(name, lastName, age);
+        System.out.println("User с именем – " + name + " добавлен в базу данных ");
     }
+
     @Override
-    public void removeUserById(long id)  {
-        try {
-            UserDaoJDBCImpl.removeUserById(id);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void removeUserById(long id) {
+        UserDaoJDBCImpl.removeUserById(id);
 
     }
+
     @Override
-    public List<User> getAllUsers()  {
+    public List<User> getAllUsers() {
         List<User> users;
-        try {
-            users = UserDaoJDBCImpl.getAllUsers();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        users = UserDaoJDBCImpl.getAllUsers();
         for (User user : users) {
             System.out.println(user);
         }
         return users;
     }
+
     @Override
-    public void cleanUsersTable()  {
-        try {
-            UserDaoJDBCImpl.cleanUsersTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void cleanUsersTable() {
+        UserDaoJDBCImpl.cleanUsersTable();
     }
 }
